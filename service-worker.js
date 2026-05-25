7403b4655308654cc334e35610043be4181df32a
sh4sh-ux <onlysh4sh@gmail.com>
2026-05-25 10:55:55 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = "receipt-db-v2-11-pwa";
+const CACHE_NAME = "receipt-db-v2-14-pwa";
 const APP_SHELL = [
   "./",
   "./index.html",
   "./manifest.json",
-  "./icons/apple-touch-icon.png",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
+  "./icons/apple-touch-icon-v2-14.png",
+  "./icons/app-icon-192-v2-14.png",
+  "./icons/app-icon-512-v2-14.png"
 ];
 
 self.addEventListener("install", (event) => {
